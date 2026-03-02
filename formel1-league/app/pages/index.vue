00b4b566ee48ac745e2e78cc2e6fe64cb4a81e15
DanielLillevik <99721997+DanielLillevik@users.noreply.github.com>
2026-03-02 17:53:22 +0100

    change team button to red

--- formel1-league/app/pages/index.vue
+++ formel1-league/app/pages/index.vue
@@ -413,7 +413,7 @@ onMounted(() => {
           </button>
           <button 
             @click="openTeamModal"
-            class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg transition-colors"
+            class="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg transition-colors"
           >
             + Add Team
           </button>
